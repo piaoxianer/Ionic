3b9e5f7079f69441c56606e0eeb2650ae02c697f
Christin <73511883+piaoxianer@users.noreply.github.com>
2023-08-24 17:47:23 -0400

    Add alert controller with delete method

--- ionic-angular-project/src/app/recipes/recipe-detail/recipe-detail.page.ts
+++ ionic-angular-project/src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+
 import { RecipesService } from '../recipes.service';
 import { Recipe } from '../recipe.model';
 
@@ -13,7 +15,9 @@ export class RecipeDetailPage implements OnInit {
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private recipesService: RecipesService
+    private recipesService: RecipesService,
+    private router: Router,
+    private alertCtrl: AlertController
   ) {}
 
   ngOnInit() {
@@ -25,5 +29,29 @@ export class RecipeDetailPage implements OnInit {
       const recipeId = paramMap.get('recipeId');
       this.loadedRecipe = this.recipesService.getRecipe(recipeId);
     });
+  }
+
+  onDeleteRecipe() {
+    this.alertCtrl
+      .create({
+        header: 'Are You Sure?',
+        message: 'Do you really want to delete the recipe?',
+        buttons: [
+          {
+            text: 'Cancel',
+            role: 'cancel',
+          },
+          {
+            text: 'Delete',
+            handler: () => {
+              this.recipesService.deleteRecipe(this.loadedRecipe.id);
+              this.router.navigate(['/recipes']);
+            },
+          },
+        ],
+      })
+      .then((alertEl) => {
+        alertEl.present();
+      });
   }
 }
